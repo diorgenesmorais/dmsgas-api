ff1cce894482d90258fe98df0138ec0cb5a7a207
Diorgenes Morais <diorgenesmorais@gmail.com>
2018-01-28 21:35:34 -0300

    Criando projeto e conectando ao Mysql

---
+++ src/test/java/com/dms/dmsgasapi/DmsgasApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dms.dmsgasapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmsgasApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
