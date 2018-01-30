359eb43a6a7c3d60d09b1167212dbb93181ae226
Diorgenes Morais <diorgenesmorais@gmail.com>
2018-01-30 16:28:27 -0300

    obtendo recurso com GET

---
+++ src/main/java/com/dms/dmsgasapi/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.dms.dmsgasapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dms.dmsgasapi.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
